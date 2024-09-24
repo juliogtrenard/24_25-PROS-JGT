5aa1c3edefeb32e5df49c6778490651f1c599d68
Julio Gonzalez <juliogtrenard@gmail.com>
2024-09-24 12:16:07 +0200

    UD1.ACT4-7.intento1.Actividad 5 terminada

--- UD1/ACT4-7/actividad5.c
+++ UD1/ACT4-7/actividad5.c
@@ -3,7 +3,28 @@
 #include <stdio.h>
 #include <sys/wait.h>
 
+void crearHijo(int cantProcesos, int numHijo) {
+    if (numHijo > cantProcesos) {
+        exit(0);
+    }
+
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        printf("Hijo %d, PID: %d, PPID: %d\n", numHijo, getpid(), getppid());
+        crearHijo(cantProcesos, numHijo + 1);
+    } else {
+        wait(NULL);
+    }
+}
+
 int main()
 {
-    
+    int n = 5;
+
+    printf("Padre, PID: %d\n", getpid());
+
+    crearHijo(n, 1);
+
+    exit(0);
 }
