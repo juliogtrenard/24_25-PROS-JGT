9068b9863e2c5fc547332ec1f1629d90d7b75c67
Julio Gonzalez <juliogtrenard@gmail.com>
2024-09-25 10:53:06 +0200

    UD1.ACT4-7.intento1.Actividad 4 comentarios hechos

--- UD1/ACT4-7/actividad4.c
+++ UD1/ACT4-7/actividad4.c
@@ -8,24 +8,29 @@ int main()
 {
     pid_t pid;
 
+    //Bucle para crear los 3 hijos
     for(int i = 1; i <= 3; i++)
     {
         switch(pid = fork())
         {
+            //Parte de los hijos
             case 0:
                 printf("Soy el hijo %d, Mi padre es %d y mi PID es %d\n", i, getppid(), getpid());
                 exit(0);
                 break;
+            //En caso de error
             case -1:
                 printf("No se ha podido crear el proceso.");
                 exit(-1);
                 break;
+            //Padre que espera a que terminen los hijos
             default:
                 wait(NULL);
                 break;
         }
     }
 
+    //Proceso padre fuera del bucle para aparecer al final
     printf("Proceso padre %d", getpid());
     exit(0);
 }
