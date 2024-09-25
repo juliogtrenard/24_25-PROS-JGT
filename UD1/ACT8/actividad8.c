56c5dcac869aa0237efdac5ce06214dc22dd0636
Julio Gonzalez <juliogtrenard@gmail.com>
2024-09-25 12:14:35 +0200

    UD1.ACT8.intento1.Estructura básica

--- UD1/ACT8/actividad8.c
+++ UD1/ACT8/actividad8.c
@@ -10,5 +10,43 @@ Como tambien de nieto -> hijo -> abuelo
 */
 void main ()
 {
+    pid_t pidHijo, pidNieto;
 
+    pidHijo = fork();
+
+    switch(pidHijo)
+    {
+        case -1:
+            printf("Error al crear el proceso.");
+            exit(-1);
+            break;
+        case 0:
+            //Hijo
+            
+            pidNieto = fork();
+            
+            switch(pidNieto)
+            {
+                case -1:
+                    printf("Error al crear el proceso.");
+                    exit(-1);
+                    break;
+                case 0:
+                    //Nieto
+
+
+                    break;
+                default:
+                    //Hijo
+
+
+                    break;
+            }
+
+            break;
+        default:
+            //Padre
+
+            break;
+    }
 }
