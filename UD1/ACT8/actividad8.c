f9471a8c1d6adc0d156eeae4e28b6a3e539314f0
Julio Gonzalez <juliogtrenard@gmail.com>
2024-09-25 12:02:45 +0200

    UD1.ACT8.intento1.Inicial

---
+++ UD1/ACT8/actividad8.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/wait.h>
+
+/*
+Programa con procesos Abuelo, hijo y nieto
+Se envian mensajes de abuelo -> hijo -> nieto
+Como tambien de nieto -> hijo -> abuelo
+*/
+void main ()
+{
+
+}
