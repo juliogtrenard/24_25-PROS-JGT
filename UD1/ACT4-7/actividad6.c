b37363c2b2e17ee53da82d0957c9d14974473a33
Julio Gonzalez <juliogtrenard@gmail.com>
2024-09-25 11:35:19 +0200

    UD1.ACT4-7.intento1.Actividad 6 comentarios hechos

--- UD1/ACT4-7/actividad6.c
+++ UD1/ACT4-7/actividad6.c
@@ -8,30 +8,38 @@ int main()
 {
     int pid, pid1, pid2;
   
+    //Se crea el hijo 1
     pid = fork();
-  
+
     if(pid == 0)
     {
+        //Hijo 1, saca su mensaje y muere
         printf("Yo soy el hijo 1, mi padre es PID = %d, yo soy PID = %d\n", getppid(), getpid());
         exit(0);
     } else {
+        //Se crea el hijo 2
         pid1 = fork();
 
         if(pid1 == 0)
         {
+            //Se crea el hijo 3
             pid2 = fork();
 
             if(pid2 == 0) {
+                //Hijo 3, saca su mensaje y muere
                 printf("Yo soy el hijo 3, mi padre es PID = %d, yo soy PID = %d\n", getppid(), getpid());
                 exit(0);
             } else {
+                //Hijo 2, espera que Hijo 3 muera, luego saca su mensaje y finaliza
                 wait(NULL);
                 printf("Yo soy el hijo 2, mi padre es PID = %d, yo soy PID = %d\n", getppid(), getpid());
                 exit(0);
             }
         } else {
+            //El padre no hace nada, espera a que finalicen los hijos
             wait(NULL);
         }
     }
+    //Finaliza el padre
     exit(0);
 }
