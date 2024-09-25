04318d88e31cd9c282004623908af81e9376a23a
Julio Gonzalez <juliogtrenard@gmail.com>
2024-09-25 11:40:13 +0200

    UD1.ACT4-7.intento1.Actividad 7 comentarios hechos

--- UD1/ACT4-7/actividad7.c
+++ UD1/ACT4-7/actividad7.c
@@ -7,21 +7,28 @@
 int main()
 {
     pid_t pid;
+
+    //Valor con el que inicia la variable
     int valor = 6;
 
     printf("Valor inicial de la variable: %d\n", valor);
 
+    //Se crea el hijo
     pid = fork();
 
+    //Estamos en el hijo
     if(pid == 0)
     {
+        //Se resta 5 a la variable, saca el mensaje y finaliza
         valor -= 5;
         printf("Variable en Proceso Hijo: %d\n", valor);
         exit(0);
-    } else {
+    } else { //Estamos en el padre
+        //Se suma 5, espera a que finalice el hijo y saca el mensaje
         valor += 5;
         wait(NULL);
         printf("Variable en Proceso Padre: %d\n", valor);
     }
+    //Finaliza el padre
     exit(0);
 }
