c229ffb23a1214f84254230edb3741c20310d728
Julio <juliogtrenard@gmail.com>
2024-10-16 10:27:37 +0200

    UD2.ACT1-3.intento1.Actividad 2 bien

--- UD2/ACT1-3/Actividad2.java
+++ UD2/ACT1-3/Actividad2.java
@@ -15,9 +15,9 @@ public class Actividad2 extends Thread {
 	public static void main(String[] args) {
 		int n = Integer.parseInt(args[0]);
 		
-        Hilos2[] hilos = new Hilos2[n];
+		Actividad2[] hilos = new Actividad2[n];
         for (int i = 0; i < n; i++) {
-            hilos[i] = new Hilos2(i + 1);
+            hilos[i] = new Actividad2(i + 1);
             hilos[i].start();
         }
         
