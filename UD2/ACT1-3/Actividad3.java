2debf249d4f187ea45e82b3b33c5133c0f38370a
Julio <juliogtrenard@gmail.com>
2024-10-16 10:34:38 +0200

    UD2.ACT1-3.intento1.Actividad 3 bien

---
+++ UD2/ACT1-3/Actividad3.java
@@ -0,0 +1,17 @@
+public class Actividad3 {
+    public static void main(String[] args) {
+        Thread hilo = new Thread();
+
+        // Mostrar el nombre y prioridad inicial del hilo
+        System.out.println("El nombre del hilo es " + hilo.getName() + " y tiene la prioridad " + hilo.getPriority() + "\n");
+
+        // Cambiar el nombre y la prioridad del hilo
+        hilo.setName("SUPER-HILO-DM2");
+        hilo.setPriority(6);
+
+        // Mostrar el nuevo nombre y prioridad del hilo
+        System.out.println("Ahora el nombre del hilo es " + hilo.getName() + " y tiene la prioridad " + hilo.getPriority() + "\n");
+
+        System.out.println("Final programa");
+    }
+}
