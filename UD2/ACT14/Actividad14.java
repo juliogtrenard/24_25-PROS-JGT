e6d08828f39bfff10b8e001c057ea71ec87038da
Julio <juliogtrenard@gmail.com>
2024-10-30 11:34:17 +0100

    UD2.ACT14.intento1.Inicial

---
+++ UD2/ACT14/Actividad14.java
@@ -0,0 +1,12 @@
+public class Actividad14 extends Thread { 
+    public static void main(String args[]) {
+        Recurso a = new Recurso(); 
+        Recurso b = new Recurso(); 
+        
+        Hilo h1 = new Hilo(a, b, "uno"); 
+        Hilo h2 = new Hilo(a, b, "dos"); 
+        
+        h1.start(); 
+        h2.start(); 
+    }
+}
