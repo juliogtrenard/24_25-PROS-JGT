c60129e4a3286dd72efcc098fac6d428db56e857
Julio <juliogtrenard@gmail.com>
2024-10-30 12:06:03 +0100

    UD2.ACT15-16.intento1.Comentarios JavaDoc en Actividad15

--- UD2/ACT15-16/Actividad15.java
+++ UD2/ACT15-16/Actividad15.java
@@ -1,13 +1,29 @@
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * Clase que representa un hilo que muestra mensajes con su nombre y la hora actual.
+ */
 class Hilo extends Thread {
+    /**
+     * Nombre del hilo
+     */
     private String nombre;
 
+    /**
+     * Constructor de la clase Hilo.
+     *
+     * @param nombre El nombre del hilo.
+     */
     public Hilo(String nombre) {
         this.nombre = nombre;
     }
 
+     /**
+     * Método que se ejecuta al iniciar el hilo.
+     * Muestra el nombre del hilo y la hora actual cinco veces,
+     * esperando 1 segundo entre cada mensaje.
+     */
     @Override
     public void run() {
         for (int i = 0; i < 5; i++) {
@@ -25,7 +41,15 @@ class Hilo extends Thread {
     }
 }
 
+/**
+ * Clase principal para ejecutar el programa.
+ */
 public class Actividad15 {
+    /**
+     * Método principal que crea e inicia los hilos.
+     *
+     * @param args Argumentos de línea de comandos.
+     */
     public static void main(String[] args) {
         Hilo hilo1 = new Hilo("Hilo 1");
         Hilo hilo2 = new Hilo("Hilo 2");
