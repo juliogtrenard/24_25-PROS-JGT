9639f5dd2716512a9907d8e76b3d43dfa63a9b25
Julio <juliogtrenard@gmail.com>
2024-10-30 11:51:14 +0100

    UD2.ACT14.intento1.Definitivo

--- UD2/ACT14/Actividad14.java
+++ UD2/ACT14/Actividad14.java
@@ -1,4 +1,20 @@
+/**
+ * Clase Actividad14 que extiende de Thread
+ * y muestra la creación y ejecución de múltiples hilos
+ * que comparten recursos.
+ * 
+ * Se crean dos instancias de Recurso
+ * y se inician dos hilos Hilo que intentan acceder a estos recursos
+ * de manera concurrente.
+ */
 public class Actividad14 extends Thread { 
+    /**
+     * Inicio de la aplicación.
+     * Se crean dos instancias de Recurso y dos hilos que 
+     * utilizan estos recursos.
+     * 
+     * @param args Argumentos de línea de comandos.
+     */
     public static void main(String args[]) {
         Recurso a = new Recurso(); 
         Recurso b = new Recurso(); 
--- UD2/ACT14/Hilo.java
+++ UD2/ACT14/Hilo.java
@@ -1,13 +1,34 @@
+/**
+ * La clase Hilo que extiende de Thread y representa
+ * un hilo que trabaja sobre dos recursos compartidos.
+ * 
+ * Los hilos sincronizan el acceso a los
+ * recursos a y b para evitar problemas de concurrencia.
+ */
 class Hilo extends Thread { 
     Recurso a; 
     Recurso b; 
     
+    /**
+     * Constructor de la clase Hilo.
+     * 
+     * @param a El primer recurso que será utilizado por el hilo.
+     * @param b El segundo recurso que será utilizado por el hilo.
+     * @param nombre El nombre del hilo, que se pasa al constructor de Thread.
+     */
     public Hilo(Recurso a, Recurso b, String nombre) { 
         super(nombre); 
         this.a = a; 
         this.b = b; 
-    } 
+    }
     
+    /**
+     * Donde se define la lógica del hilo.
+     * 
+     * Realiza la sincronización en el recurso a,
+     * duerme el hilo durante 100ms y luego intenta acceder
+     * al recurso b.
+     */
     public void run() { 
         System.out.println("Hilo " + this.getName() + " comienza"); 
         
@@ -26,5 +47,11 @@ class Hilo extends Thread {
     } 
 }
 
+/**
+ * La clase Recurso representa un recurso compartido
+ * que puede ser utilizado por múltiples hilos.
+ * 
+ * En este programa la clase está vacía.
+ */
 class Recurso { 
 }
